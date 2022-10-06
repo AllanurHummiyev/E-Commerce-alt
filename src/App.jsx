9ca7b4bc8c0c34a069b66bcd4a32b4f4cb177f56
Allanur Hummiyev <allanurhummiyev@gmail.com>
2022-10-06 16:04:58 +0700

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./pages/Home";
+import React from "react";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+
+const App = () => {
+  return <Product />;
+};
+
+export default App;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+`;
+
+const Title = styled.div``;
+
+const Description = styled.p``;
+
+const Price = styled.span``;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Description>
+          <Price>$20</Price>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
