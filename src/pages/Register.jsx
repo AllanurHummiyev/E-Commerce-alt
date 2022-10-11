9decf21248e8216035a7096f223c72aa7cb06f32
Allanur Hummiyev <allanurhummiyev@gmail.com>
2022-10-11 23:37:49 +0700

    commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,6 +10,7 @@ const Container = styled.div`
     ),
     url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
       center;
+  background-size: cover;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -21,15 +22,36 @@ const Wrapper = styled.div`
   background-color: white;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
 
-const Agreement = styled.span``;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
 
 const Register = () => {
   return (
